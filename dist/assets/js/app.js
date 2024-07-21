e91aa221d1e4234c4edf7c6679468e2084678697
hassanosamaa <almsryh877@gmail.com>
2024-07-21 17:49:18 +0300

    add validation in contact page

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -19152,8 +19152,43 @@
 	  });
 	};
 
+	const validation = () => {
+	  var inputEle = document.querySelectorAll('.contact input');
+
+	  for (var i = 0; i < inputEle.length; i++) {
+	    inputEle[i].addEventListener("input", e => {
+	      Regex(e.target);
+	    });
+	  }
+
+	  function Regex(e) {
+	    var regex = {
+	      nameInput: /^[a-z ]+$/gi,
+	      phoneInput: /^01(1|2|5|0)[0-9]{8}$/gi,
+	      timeInput: /^[0-9]{4}$/gi,
+	      emailInput: /^[0-9a-zA-Z]+\@(gmail|yahoo)\.com$/gi,
+	      detailsInput: /^.$/gi
+	    };
+	    console.log(e.value);
+
+	    if (regex[e.id].test(e.value)) {
+	      e.classList.add("is-valid");
+	      e.classList.remove("is-invalid");
+	    } else {
+	      e.classList.remove("is-valid");
+	      e.classList.add("is-invalid");
+	    }
+
+	    if (e.value == "") {
+	      e.classList.remove("is-valid");
+	      e.classList.remove("is-invalid");
+	    }
+	  }
+	};
+
 	jquery(document).ready(() => {
 	  portofolio();
+	  validation();
 	});
 
 })));
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
 import 'bootstrap';
 import {portofolio} from "./portofolio";
+import {validation} from "./validation";
 
 
 
 $(document).ready(() => {
     portofolio();
+    validation();
 
 });
